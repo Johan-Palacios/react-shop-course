f11b31b602ffe038541e233ddde28c3a9aee6bf0
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-11-30 23:50:42 -0600

    Remove extra code

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import "@styles/RecoveryPassword.scss";
-import email_logo from "@images/icons/email.svg";
 import logo_yard_sale from "@images/logos/logo_yard_sale.svg";
 
 const NotFound = () => {
