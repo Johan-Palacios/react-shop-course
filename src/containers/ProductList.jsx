e36a089e5ad871abc4ac9f297cfa777897d78ccb
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-12-04 23:01:21 -0600

    Product Item and List Added

--- src/containers/ProductList.jsx
+++ src/containers/ProductList.jsx
@@ -1,15 +1,21 @@
-import React from 'react';
-import ProductItem from '@components/ProductItem';
-import '@styles/ProductList.scss';
+import React, { useState } from "react";
+import useGetProducts from "@hooks/useGetProducts";
+import ProductItem from "@components/ProductItem";
+import "@styles/ProductList.scss";
+
+const API = "https://api.escuelajs.co/api/v1/products";
 
 const ProductList = () => {
-	return (
-		<section className="main-container">
-			<div className="ProductList">
-				<ProductItem />
-			</div>
-		</section>
-	);
-}
+  const products = useGetProducts(API);
+  return (
+    <section className="main-container">
+      <div className="ProductList">
+        {products.map((product) => (
+          <ProductItem product={product} key={product.id} />
+        ))}
+      </div>
+    </section>
+  );
+};
 
 export default ProductList;
