d4092c473b51b4d61d9d9727cbe2ef7b4fa918bb
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-12-04 23:01:59 -0600

    Add login useRef

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,19 +1,32 @@
-import React from "react";
+import React, { useRef } from "react";
 import "@styles/Login.scss";
 import logo_yard_sale from "@images/logos/logo_yard_sale.svg";
 
 const Login = () => {
+  const form = useRef(null);
+
+  const handleSubmit = () => {
+    event.preventDefault();
+    const formData = new FormData(form.current);
+    const data = {
+      username: formData.get("email"),
+      password: formData.get("password"),
+    };
+    console.log(data);
+    return data;
+  };
+
   return (
     <div className="Login">
       <div className="Login-container">
         <img src={logo_yard_sale} alt="logo" className="logo" />
-        <form action="/" className="form">
+        <form action="/" className="form" ref={form}>
           <label htmlFor="email" className="label">
             Email address
           </label>
           <input
             type="text"
-            id="email"
+            name="email"
             placeholder="platzi@example.cm"
             className="input input-email"
           />
@@ -22,15 +35,18 @@ const Login = () => {
           </label>
           <input
             type="password"
-            id="password"
+            name="password"
             placeholder="*********"
             className="input input-password"
           />
-          <input
+          <button
             type="submit"
             value="Log in"
             className="primary-button login-button"
-          />
+            onClick={handleSubmit}
+          >
+            Log In
+          </button>
           <a href="/">Forgot my password</a>
         </form>
         <button className="secondary-button signup-button">Sign up</button>
