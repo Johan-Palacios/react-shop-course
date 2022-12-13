c759d5df67c7abbcfffb576b11c0775e65b58efb
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-12-12 23:30:58 -0600

    Prevent no image

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,16 +2,23 @@ import React, { useContext, useState } from "react";
 import AppContext from "@context/AppContext";
 import "@styles/ProductItem.scss";
 import add_cart from "@images/icons/bt_add_to_cart.svg";
+import close_icon from "@images/icons/icon_close.png";
 
 const ProductItem = ({ product }) => {
   const { addToCart } = useContext(AppContext);
   const handleClick = (item) => {
     addToCart(item);
   };
+  const validateImage = (image) => {
+    if (/^https:\/\/api\.lorem\.space\/image*/i.test(image)) {
+      return image;
+    }
+    return close_icon;
+  };
 
   return (
     <div className="ProductItem">
-      <img src={product.images[0]} alt={product.title} />
+      <img src={validateImage(product.images[0])} alt={product.title} />
       <div className="product-info">
         <div>
           <p>${product.price}</p>
