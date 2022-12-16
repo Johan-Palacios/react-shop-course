8ea8b5ec01ead9f3079e983b321008a4a1a5a962
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-12-15 23:05:36 -0600

    Fix issue

--- src/containers/MyOrder.jsx
+++ src/containers/MyOrder.jsx
@@ -1,4 +1,4 @@
-import React, { useContext } from "react";
+import React, { useContext, useState } from "react";
 import OrderItem from "@components/OrderItem";
 import AppContext from "@context/AppContext";
 import "@styles/MyOrder.scss";
