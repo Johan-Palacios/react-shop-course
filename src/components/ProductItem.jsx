263fa2365c887cca81e52974064c6918c1e2f2ce
Johan Palacios <johanpalacios.rivera@gmail.com>
2022-12-15 23:04:56 -0600

    [ADD]: Toggle Item added

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -2,10 +2,13 @@ import React, { useContext, useState } from "react";
 import AppContext from "@context/AppContext";
 import "@styles/ProductItem.scss";
 import add_cart from "@images/icons/bt_add_to_cart.svg";
+import added_cart from "@images/icons/bt_added_to_cart.svg";
 import close_icon from "@images/icons/icon_close.png";
 
 const ProductItem = ({ product }) => {
-  const { addToCart } = useContext(AppContext);
+  const { addToCart, state } = useContext(AppContext);
+  const itsProductAdded = () =>
+    state.cart.some((item) => item.id === product.id);
   const handleClick = (item) => {
     addToCart(item);
   };
@@ -25,7 +28,11 @@ const ProductItem = ({ product }) => {
           <p>{product.title}</p>
         </div>
         <figure onClick={() => handleClick(product)}>
-          <img src={add_cart} alt="Add to cart" />
+          {itsProductAdded() ? (
+            <img src={added_cart} alt="Addeed to cart" />
+          ) : (
+            <img src={add_cart} alt="Add to cart" />
+          )}
         </figure>
       </div>
     </div>
